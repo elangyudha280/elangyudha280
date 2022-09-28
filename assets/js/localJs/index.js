f192e602f9c72940eb88833e1ed426215e16fb4c
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-09-28 21:19:19 +0700

    selesai desain portfolio programing

---
+++ assets/js/localJs/index.js
@@ -0,0 +1,22 @@
+
+
+// swipper programig
+const swiper = new Swiper(".swipper-programing", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    grabCursor:true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+    },
+  });
