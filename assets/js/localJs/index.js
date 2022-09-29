56ed2f8d787693c18b74785570ad520a4ec96d3f
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-09-29 11:36:32 +0700

    selesai desain portfolio photography

--- assets/js/localJs/index.js
+++ assets/js/localJs/index.js
@@ -1,7 +1,28 @@
 
 
-// swipper programig
+// swipper programing
 const swiper = new Swiper(".swipper-programing", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    grabCursor:true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+    },
+  });
+
+  // swipper phtograpy
+  const swiperPhotgrapy = new Swiper(".swipper-photography", {
     slidesPerView: 1,
     spaceBetween: 10,
     grabCursor:true,
