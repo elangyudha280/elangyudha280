44146bfa86a04dbade711ed165dd021e582652a8
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-09-29 20:33:47 +0700

    fix bugs

--- assets/js/localJs/index.js
+++ assets/js/localJs/index.js
@@ -1,51 +1,45 @@
-
-
 // code aos js
 
 AOS.init({
-  duration:700,
-  offset:0
-})
-
-
-
-
+  duration: 700,
+  offset: 100,
+});
 
 // swipper programing
-const swiper = new Swiper(".swipper-programing", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    grabCursor:true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
+const swiper = new Swiper('.swipper-programing', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
-  });
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
 
-  // swipper phtograpy
-  const swiperPhotgrapy = new Swiper(".swipper-photography", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    grabCursor:true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-     
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
+// swipper phtograpy
+const swiperPhotgrapy = new Swiper('.swipper-photography', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
-  });
+
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
